b82eac900ea9c98194398a413ebfc2e403b4cd1b
史博 <312685889@qq.com>
2019-06-23 23:40:41 +0800

    更新表格及数据

--- manager/src/main/resources/static/sql/financial.sql
+++ manager/src/main/resources/static/sql/financial.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
+Source Server         : localhost_3306
 Source Server Version : 50726
 Source Host           : localhost:3306
 Source Database       : financial
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50726
 File Encoding         : 65001
 
-Date: 2019-06-20 11:11:50
+Date: 2019-06-23 23:35:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -37,6 +37,10 @@ CREATE TABLE `order_t` (
 -- ----------------------------
 -- Records of order_t
 -- ----------------------------
+INSERT INTO `order_t` VALUES ('1561213922827', 'aaa', 'aaa', 'aaa', '0', '2', 'aaa', '23.000', null, '2019-06-22 22:15:38', '2019-06-22 22:15:38');
+INSERT INTO `order_t` VALUES ('1561287648356', 'aaa', 'aaa', 'aaa', '0', '2', 'aaa', '444.000', null, '2019-06-23 18:55:05', '2019-06-23 18:55:05');
+INSERT INTO `order_t` VALUES ('1561298069689', 'bbb', '1560996630074', 'bbb', '0', '2', 'bbb', '123.000', null, '2019-06-23 21:45:59', '2019-06-23 21:45:59');
+INSERT INTO `order_t` VALUES ('1561298143649', 'aaa', '1560996630074', 'bbb', '0', '2', 'bbb', '123.000', null, '2019-06-23 21:46:23', '2019-06-23 21:46:23');
 
 -- ----------------------------
 -- Table structure for product
@@ -65,3 +69,46 @@ INSERT INTO `product` VALUES ('1560996630074', '健康基金', '15.000', '0.000'
 INSERT INTO `product` VALUES ('1560996861552', '财富基金', '13.000', '0.000', '0', '29.000', '0', '一夜暴富的基金', '财富基金会', null, '2019-06-20 10:02:26', '2019-06-20 10:02:26');
 INSERT INTO `product` VALUES ('1560997025457', '石油基金', '15.000', '0.000', '0', '20.000', '0', null, '石化集团', null, '2019-06-20 10:04:43', '2019-06-20 10:04:43');
 INSERT INTO `product` VALUES ('aaa', 'user', '12.000', '0.000', '0', '20.000', '0', '是是是', 'user', 'user', '2019-06-19 10:01:11', '2019-06-19 10:01:11');
+
+-- ----------------------------
+-- Table structure for sign
+-- ----------------------------
+DROP TABLE IF EXISTS `sign`;
+CREATE TABLE `sign` (
+  `id` varchar(20) NOT NULL,
+  `private_key` varchar(1000) NOT NULL COMMENT '私钥',
+  `public_key` varchar(1000) NOT NULL COMMENT '公钥',
+  `auth_id` varchar(255) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='签名表';
+
+-- ----------------------------
+-- Records of sign
+-- ----------------------------
+INSERT INTO `sign` VALUES ('1561287982801', 'MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAJySGcjdyLsUWMN1kywQpyksGlHoX5J/CYZtSiAH6m7otN6CbYxMc6JP6cBHFLJqOKKro+N+f/tExp32uhnXsr28ReuIrHYDcM2I4fGB+6tAdPySo7/kYiDix3ATzLpahQvAiBKXegfb3zjjE5/7kDKNMuwCRzWVNDcj8GjgMKdPAgMBAAECgYAyYe08LuomEUES/KW0O1OfMxrxGoaOnKSTBl4g+oscz3NMWsonqH5ZT7/XaMgnNXkuF29wiEdkstf7AucV48wXMl1zuy2Rc+i5N4WVq/c7kTEuYZpf4VWm9oOZNYaGpG/WM6YzdB6RREiSf9DrMWPDdukBr3kDIQASdFQ4ucTsQQJBAOE2BWW2MNx+ZmsUArHH6tuawzhODQDi1p+FlRjZGLmXdoWRLxQRWINBSRucBu49VNdoL9jtUUt36CJ7xLVOGuECQQCx+crpyJVTG0X8bKwwB2B9jvSAvuCrIRZtUwX5p/FF881wtg2MAMtggl8dF0ARt/MUKsvKcJKB2hSD1S6siLgvAkEAqQvspzlHmeqDJDMEBsBiaO7VRKlJl3Wt1lXl4Z3rsjuf6ohdBzox72wM3V2T0jZVPc9lwvX4BKb/pwqyT8lN4QJBAIwSyT0OfVgXUxHEhhCW/b5UUOMzV6fYNsp8WWuQXjhsFfjrj+XsiiS+9xTyQ56uZEYvGI2P4/8HILZRhpaMVEkCQQClPNa4mHvWWMglx/i/919gVTF1pKKnQ7Z/rLWhnxvFeiC/m7bDTCu1nUAw8B8dQ6yncZJfxoIR59nBtyUTRfgt', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCckhnI3ci7FFjDdZMsEKcpLBpR6F+SfwmGbUogB+pu6LTegm2MTHOiT+nARxSyajiiq6Pjfn/7RMad9roZ17K9vEXriKx2A3DNiOHxgfurQHT8kqO/5GIg4sdwE8y6WoULwIgSl3oH29844xOf+5AyjTLsAkc1lTQ3I/Bo4DCnTwIDAQAB', '1000', null, '2019-06-23 18:55:02', '2019-06-23 18:55:02');
+INSERT INTO `sign` VALUES ('1561298210602', 'MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBALImiVg9mO8BVW3wyVpGR49JaoAHvj94e5Frsa/l/DQX247UhYF+PCw0lehg40nw+/Qy2pZkPku6gIaoWEubT3f8B6PoI1h1q9ytFmNG0RThfWciAEX+6+DU3br9O9CsHVh0VcvN+Fzjx4z71qXrr/uyg0U4O0PI5EFaGo39/RjzAgMBAAECgYA94Iq13nc/FJyCmi6lr2FtkFdwbsAStZtd0CUgbmZiBaNaAdD5QEpQJFepJfXsGcq5b+xibiEBFp9Lpi88244HDzRZ5pYj6hTY5F3BPenJ8UfUiS+51j2n/8Hf0RKs6ysl795IGkC121Q3kVyzoyzDUTFK2ST0sGjJgXbQTHOXuQJBAOC9krkoi/aCFoU0jUVDjTlGMZYjoPzWmUnIW8mipRfJK08RoixbUyeA6AVJ2aK979HSgTiu0pcbeGdSQzt73S0CQQDK7gPKM3De1puY6WL7gIIfhOl8vm/KecvfxQtQ2xABU9C2PXis+Wzh8GXDdHYBKr6sFafpSHiljHAqxjPwIeKfAkEAgzVqkftgK50rdL2HtOaJbuTSUmues76+Y9s7626voEFsrY8H9Ymj9K5Hx76j92WYvBGALA21GU+KbhgS9ImOSQJAJEfbi5oY0u5hkTwgw0Bdg08bA390symFZrNu63zO5r3accqZuas6aUa2mvRGaL0Aq/mPNKkNUOPD5hHblVIplwJAO2IaPhaK0ViudJ98ioCQ7DFGR1JuyZL1ZCNmR4FrnOZ2CogmyWFLk3FCqeYQGyloKP2e66aJXFi0AtCoGEGKAw==', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCyJolYPZjvAVVt8MlaRkePSWqAB74/eHuRa7Gv5fw0F9uO1IWBfjwsNJXoYONJ8Pv0MtqWZD5LuoCGqFhLm093/Aej6CNYdavcrRZjRtEU4X1nIgBF/uvg1N26/TvQrB1YdFXLzfhc48eM+9al66/7soNFODtDyORBWhqN/f0Y8wIDAQAB', '1', null, '2019-06-23 21:44:46', '2019-06-23 21:44:46');
+
+-- ----------------------------
+-- Table structure for verification_order
+-- ----------------------------
+DROP TABLE IF EXISTS `verification_order`;
+CREATE TABLE `verification_order` (
+  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '订单编号',
+  `chan_id` varchar(64) NOT NULL COMMENT '渠道编号',
+  `product_id` varchar(64) NOT NULL COMMENT '产品编号',
+  `chan_user_id` varchar(64) NOT NULL COMMENT '渠道用户编号',
+  `order_type` varchar(64) NOT NULL COMMENT '类型，APPLY:申购,REDEEM:赎回',
+  `outer_order_id` varchar(64) NOT NULL COMMENT '外部订单编号',
+  `amount` decimal(15,3) NOT NULL COMMENT '金额',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='订单验证';
+
+-- ----------------------------
+-- Records of verification_order
+-- ----------------------------
